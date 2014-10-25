fd567ebee8c50c74982186e0a63c8b7ac94ffe80
SmallTigerBrother <664674103@qq.com>
2014-10-26 00:07:12 +0800

    新增RootView方法

--- LayoutIDE/src/com/tiger/layoutide/ide/ViewGroupHelper.java
+++ LayoutIDE/src/com/tiger/layoutide/ide/ViewGroupHelper.java
@@ -1,14 +1,27 @@
 package com.tiger.layoutide.ide;
 
-import com.tiger.layoutide.tree.IViewGroup;
+import com.tiger.layoutide.widget.IViewGroup;
 
 import android.view.ViewGroup;
 
 public class ViewGroupHelper extends ViewHelper implements IViewGroup
 {
+	private boolean isRootViewGroup = false;
+	
 	public ViewGroupHelper(ViewGroup viewGroup)
 	{
 		super(viewGroup);
 	}
 
+	@Override
+	public boolean isRootViewGroup()
+	{
+		return isRootViewGroup;
+	}
+
+	@Override
+	public void setRootViewGroup(boolean isRootView)
+	{
+		this.isRootViewGroup = isRootView;
+	} 
 }
