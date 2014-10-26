bfb245099f5ed43a2b419d7c87d442e7ea09ee53
SmallTigerBrother <664674103@qq.com>
2014-10-26 22:49:22 +0800

    实现垃圾箱功能

--- LayoutIDE/src/com/tiger/layoutide/MainActivity.java
+++ LayoutIDE/src/com/tiger/layoutide/MainActivity.java
@@ -5,36 +5,48 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.View.OnDragListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.mn.tiger.annonation.ViewById;
 import com.mn.tiger.utility.LogTools;
 import com.mn.tiger.utility.ViewInjector;
 import com.tiger.layoutide.ide.Emulator;
+import com.tiger.layoutide.ide.ExampleViewPanel;
 import com.tiger.layoutide.ide.PropertiesToolBar;
-import com.tiger.layoutide.widget.TGTextView;
 
 public class MainActivity extends Activity
 {
+	@ViewById(id = R.id.main_view)
+	private RelativeLayout mainView;
+	
 	@ViewById(id = R.id.emulator_screen)
 	private Emulator emulatorLayout;
 	
 	@ViewById(id = R.id.properties_tool_bar)
 	private ScrollView propertiesToolBarView;
-
-	@ViewById(id = R.id.test_btn)
-	private Button testButton;
 	
+	@ViewById(id = R.id.example_panel)
+	private LinearLayout examplePanelLayout;
+
 	@ViewById(id = R.id.output_xml)
 	private Button outputButton;
 	
+	@ViewById(id = R.id.garbage_bin)
+	private TextView garbageBin;
+	
 	private PropertiesToolBar propertiesToolBar;
+	
+	private ExampleViewPanel exampleViewPanel;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -45,6 +57,8 @@ public class MainActivity extends Activity
 		ViewInjector.initInjectedView(this, this);
 		
 		propertiesToolBar = new PropertiesToolBar(propertiesToolBarView);
+		
+		exampleViewPanel = new ExampleViewPanel(examplePanelLayout);
 		
 		outputButton.setOnClickListener(new OnClickListener()
 		{
@@ -73,33 +87,31 @@ public class MainActivity extends Activity
 			}
 		});
 		
-		testButton.setOnClickListener(new OnClickListener()
+		mainView.setOnClickListener(new OnClickListener()
 		{
 			@Override
 			public void onClick(View v)
 			{
-				final TGTextView textView = new TGTextView(MainActivity.this);
-				
-				LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-						LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-				
-				textView.setLayoutParams(layoutParams);
-				
-				textView.setText("text");
-				textView.setBackgroundColor(Color.BLACK);
-				
-				textView.setOnClickListener(new OnClickListener()
-				{
-					@Override
-					public void onClick(View v)
-					{
-						propertiesToolBar.setSelectedView(textView);
-					}
-				});
-				
-				emulatorLayout.addView(textView);
+				propertiesToolBar.setSelectedView(null);
 			}
 		});
 		
+		garbageBin.setOnDragListener(new OnDragListener()
+		{
+			@Override
+			public boolean onDrag(View v, DragEvent event)
+			{
+				if(event.getAction() == DragEvent.ACTION_DROP)
+				{
+					View curDragView = (View)Emulator.getSingleInstance().getCurDragView();
+					if(null != curDragView)
+					{
+						((ViewGroup)curDragView.getParent()).removeView(curDragView);
+						Emulator.getSingleInstance().setCurDragView(null);
+					}
+				}
+				return true;
+			}
+		});
 	}
 }
