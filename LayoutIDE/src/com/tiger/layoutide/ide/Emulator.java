363509e4a90d90f32cc398fad5c0f44d9eaa4ceb
SmallTigerBrother <664674103@qq.com>
2014-10-26 22:47:11 +0800

    新增newViewMode、curDragView全局变量

--- LayoutIDE/src/com/tiger/layoutide/ide/Emulator.java
+++ LayoutIDE/src/com/tiger/layoutide/ide/Emulator.java
@@ -3,19 +3,52 @@ package com.tiger.layoutide.ide;
 import android.content.Context;
 import android.util.AttributeSet;
 
+import com.tiger.layoutide.widget.IView;
 import com.tiger.layoutide.widget.TGLinearLayout;
 
 public class Emulator extends TGLinearLayout
 {
+	private static Emulator emulator;
+	
+	private IView newViewModel;
+	
+	private IView curDragView;
+	
 	public Emulator(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
 		setRootViewGroup(true);
+		emulator = this;
+	}
+	
+	public static Emulator getSingleInstance()
+	{
+		return emulator;
 	}
 
 	@Override
 	public String getXMLString()
 	{
 		return super.getXMLString();
+	}
+	
+	public void setNewViewModel(IView view)
+	{
+		this.newViewModel = view;
+	}
+	
+	public IView getNewViewModel()
+	{
+		return newViewModel;
+	}
+
+	public IView getCurDragView()
+	{
+		return curDragView;
+	}
+
+	public void setCurDragView(IView curDragView)
+	{
+		this.curDragView = curDragView;
 	}
 }
