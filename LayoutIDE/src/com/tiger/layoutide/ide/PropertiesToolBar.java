d3658b341f065ba4117fbe07bfae471dc0f3db07
SmallTigerBrother <664674103@qq.com>
2014-10-27 22:34:26 +0800

    新增Relave属性控制视图

--- LayoutIDE/src/com/tiger/layoutide/ide/PropertiesToolBar.java
+++ LayoutIDE/src/com/tiger/layoutide/ide/PropertiesToolBar.java
@@ -1,5 +1,7 @@
 package com.tiger.layoutide.ide;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -7,6 +9,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,7 +18,9 @@ import android.widget.Toast;
 import com.mn.tiger.annonation.ViewById;
 import com.mn.tiger.utility.ViewInjector;
 import com.tiger.layoutide.R;
+import com.tiger.layoutide.utils.Constant;
 import com.tiger.layoutide.widget.IView;
+import com.tiger.layoutide.widget.TGRelativeLayout;
 
 public class PropertiesToolBar
 {
@@ -28,14 +33,13 @@ public class PropertiesToolBar
 	@ViewById(id = R.id.id_editor)
 	private EditText idEditText;
 	
+	/*********************************** Common Position *********************************/
+	
 	@ViewById(id = R.id.layout_width_editor)
 	private EditText layoutWidthEditText;
 	
 	@ViewById(id = R.id.layout_height_eidtor)
 	private EditText layoutHeightEditText;
-	
-	@ViewById(id = R.id.layout_weight_editor)
-	private EditText layoutWeightEditText;
 	
 	@ViewById(id = R.id.layout_marginleft_editor)
 	private EditText marginLeftEditText;
@@ -49,8 +53,17 @@ public class PropertiesToolBar
 	@ViewById(id = R.id.layout_marginBottom_editor)
 	private EditText marginBottomEditText;
 	
+	/*********************************** LinearLayout Layout *********************************/
+	
+	@ViewById(id = R.id.layout_weight_editor)
+	private EditText layoutWeightEditText;
+	
+	/*********************************** Background *********************************/
+	
 	@ViewById(id = R.id.background_color_editor)
 	private EditText backgroundColorEditText;
+	
+	/*********************************** Content *********************************/
 	
 	@ViewById(id = R.id.text_editor)
 	private EditText textEditText;
@@ -63,6 +76,54 @@ public class PropertiesToolBar
 	
 	@ViewById(id = R.id.gravity_selector)
 	private Spinner gravitySelector;
+	
+	
+	/*********************************** Relativie Position *********************************/
+	
+	@ViewById(id = R.id.align_parent_left_selector)
+	private Spinner alignParentLeftSelector;
+	
+	@ViewById(id = R.id.align_parent_right_selector)
+	private Spinner alignParentRightSelector;
+	
+	@ViewById(id = R.id.align_parent_top_selector)
+	private Spinner alignParentTopSelector;
+	
+	@ViewById(id = R.id.align_parent_bottom_selector)
+	private Spinner alignParentBottomSelector;
+	
+	@ViewById(id = R.id.align_left_selector)
+	private Spinner alignLeftSelector;
+	
+	@ViewById(id = R.id.align_right_selector)
+	private Spinner alignRightSelector;
+	
+	@ViewById(id = R.id.align_top_selector)
+	private Spinner alignTopSelector;
+	
+	@ViewById(id = R.id.align_bottom_selector)
+	private Spinner alignBottomSelector;
+	
+	@ViewById(id = R.id.below_selector)
+	private Spinner belowSelector;
+	
+	@ViewById(id = R.id.above_selector)
+	private Spinner aboveSelector;
+	
+	@ViewById(id = R.id.toleftof_selector)
+	private Spinner toLeftOfSelector;
+	
+	@ViewById(id = R.id.torightof_selector)
+	private Spinner toRightOfSelector;
+	
+	@ViewById(id = R.id.center_in_parent_selector)
+	private Spinner centerInParentSelector;
+	
+	@ViewById(id = R.id.center_vertical_selector)
+	private Spinner centerVerticalSelector;
+	
+	@ViewById(id = R.id.center_horizontal_selector)
+	private Spinner centerHorizontalSelector;
 	
 	private IView selectedView = null;
 	
@@ -112,16 +173,18 @@ public class PropertiesToolBar
 			}
 		});
 		
-		initPositionPropertyEditors();
+		initCommonPositionPropertyEditors();
 		
 		initBackgroundPropertyEditors();
 		
 		initContentPropertyEditors();
 		
+		initRelativePositionPropertiesEditors();
+		
 		propertiesToolBar = this;
 	}
 	
-	private void initPositionPropertyEditors()
+	private void initCommonPositionPropertyEditors()
 	{
 		layoutWidthEditText.addTextChangedListener(new TextWatcher()
 		{
@@ -508,6 +571,422 @@ public class PropertiesToolBar
 		});
 	}
 	
+	private void initRelativePositionPropertiesEditors()
+	{
+		alignParentLeftSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentLeft(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentLeft(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignParentRightSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentRight(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentRight(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignParentTopSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentTop(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentTop(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignParentBottomSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentBottom(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setAlignParentBottom(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		centerInParentSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setCenterInParent(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setCenterInParent(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		centerVerticalSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setCenterVertical(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setCenterVertical(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		centerHorizontalSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(Constant.TRUE.equals(parent.getAdapter().getItem(position).toString()))
+				{
+					if(null != selectedView)
+					{
+						selectedView.setCenterHorizontal(Constant.TRUE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+				{
+					if(null != selectedView)
+					{
+						selectedView.setCenterHorizontal(Constant.FALSE);
+					}
+					else
+					{
+						Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		belowSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setBelow(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		aboveSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setAbove(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		toLeftOfSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setToLeftOf(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		toRightOfSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setToRightOf(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignLeftSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setAlignLeft(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignRightSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setAlignRight(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignTopSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setAlignTop(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+		
+		alignBottomSelector.setOnItemSelectedListener(new OnItemSelectedListener()
+		{
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+			{
+				if(null != selectedView)
+				{
+					selectedView.setAlignBottom(parent.getAdapter().getItem(position).toString());
+				}
+				else
+				{
+					Toast.makeText(context, "Please select one View before edit the property", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent)
+			{
+			}
+		});
+	}
+	
 	public void setSelectedView(IView selectedView)
 	{
 		defaultSelectedViewChanagedListener.onSelectedViewChanaged(this.selectedView, selectedView);
@@ -526,6 +1005,8 @@ public class PropertiesToolBar
 			{
 				idEditText.setText("");
 			}
+			
+			/*********************************** Common Position *********************************/
 			
 			if(!TextUtils.isEmpty(selectedView.getLayoutWidth()))
 			{
@@ -600,6 +1081,8 @@ public class PropertiesToolBar
 				marginBottomEditText.setText("");
 			}
 			
+			/*********************************** Background *********************************/
+			
 			if(!TextUtils.isEmpty(selectedView.getBackgroundColor()))
 			{
 				backgroundColorEditText.setText(selectedView.getBackgroundColor());
@@ -608,6 +1091,8 @@ public class PropertiesToolBar
 			{
 				backgroundColorEditText.setText("");
 			}
+			
+			/*********************************** Content *********************************/
 			
 			if(!TextUtils.isEmpty(selectedView.getText()))
 			{
@@ -635,6 +1120,44 @@ public class PropertiesToolBar
 			{
 				textColorEditText.setText("");
 			}
+			
+			/*********************************** Relativie Position *********************************/
+			
+			if(((View)selectedView).getParent() instanceof TGRelativeLayout)
+			{
+				if(!TextUtils.isEmpty(selectedView.getAlignParentLeft()))
+				{
+					// 1 为true
+					alignParentLeftSelector.setSelection(1);
+				}
+				
+				ArrayList<CharSequence> parentChildIdList = ((TGRelativeLayout)((View)selectedView).getParent()).getChildIdList();
+				//移除自身的id
+				if(null != selectedView.getIdName())
+				{
+					parentChildIdList.remove(selectedView.getIdName());
+				}
+				CharSequence[] parentChildIds = new CharSequence[parentChildIdList.size()];
+				parentChildIdList.toArray(parentChildIds);
+				
+				ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(context,
+						android.R.layout.simple_spinner_item, parentChildIds);
+				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				
+				
+				belowSelector.setAdapter(adapter);
+				if(!TextUtils.isEmpty(selectedView.getBelow()))
+				{
+					belowSelector.setSelection(parentChildIdList.indexOf(selectedView.getBelow()));
+				}
+				
+				aboveSelector.setAdapter(adapter);
+				if(!TextUtils.isEmpty(selectedView.getAbove()))
+				{
+					belowSelector.setSelection(parentChildIdList.indexOf(selectedView.getAbove()));
+				}
+			}
+			
 		}
 	}
 	
