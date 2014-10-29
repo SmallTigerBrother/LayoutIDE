3b9ca78f458fb06272311d0249edf6085d295f55
SmallTigerBrother <664674103@qq.com>
2014-10-29 22:10:17 +0800

    新增相对属性值

---
+++ LayoutIDE/src/com/tiger/layoutide/utils/RelativeValues.java
@@ -0,0 +1,30 @@
+package com.tiger.layoutide.utils;
+
+public class RelativeValues
+{
+	public static final String ALIGN_PARENT_NONE = "NONE";
+	
+	public static final String ALIGN_PARENT_LEFT = "left";
+	
+	public static final String ALIGN_PARENT_RIGHT = "right";
+	
+	public static final String ALIGN_PARENT_TOP = "top";
+	
+	public static final String ALIGN_PARENT_BOTTOM = "bottom";
+	
+	public static final String ALIGN_PARENT_LEFT_AND_TOP = "left|top";
+	
+	public static final String ALIGN_PARENT_LEFT_AND_BOTTOM = "left|bottom";
+	
+	public static final String ALIGN_PARENT_RIGHT_AND_TOP = "right|top";
+	
+	public static final String ALIGN_PARENT_RIGHT_AND_BOTTOM = "right|bottom";
+	
+	public static final String CENTER_NONE = "NONE";
+	
+	public static final String CENTER_IN_PARENT = "center_in_parent";
+	
+	public static final String CENTER_HORIZONTAL = "center_horizontal";
+	
+	public static final String CENTER_VERTICAL = "center_vertical";
+}
