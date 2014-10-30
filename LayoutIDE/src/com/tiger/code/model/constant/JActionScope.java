f2ef414930e90d995b6ba3cf2a1a733f90ce877a
tiger <664674103@qq.com>
2014-10-30 18:09:38 +0800

    迁移代码，新增JModifier

---
+++ LayoutIDE/src/com/tiger/code/model/constant/JActionScope.java
@@ -0,0 +1,12 @@
+package com.tiger.code.model.constant;
+
+public class JActionScope 
+{
+    public static final String PUBLIC = "public ";
+	
+	public static final String PROTECTED = "protected ";
+	
+	public static final String PRIVATE = "private ";
+	
+	public static final String DEFAULT = "";
+}
