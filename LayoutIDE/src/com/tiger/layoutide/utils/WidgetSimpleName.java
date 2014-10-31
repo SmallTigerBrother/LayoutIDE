c3e36eb5d6c91e5835430d470d1e78f36d6afcfd
tiger <664674103@qq.com>
2014-10-31 11:47:11 +0800

    新增widget简称

---
+++ LayoutIDE/src/com/tiger/layoutide/utils/WidgetSimpleName.java
@@ -0,0 +1,24 @@
+package com.tiger.layoutide.utils;
+
+public class WidgetSimpleName
+{
+	public static final String VIEW = "View";
+	
+	public static final String VIEWGROUP = "ViewGroup";
+	
+	public static final String TEXT_VIEW = "TextView";
+	
+	public static final String BUTTON = "Button";
+	
+	public static final String EDIT_TEXT = "EditText";
+	
+	public static final String IMAGE_VIEW = "ImageView";
+	
+	public static final String CHECK_BOX = "CheckBox";
+	
+	public static final String LINEAR_LAYOUT = "LinearLayout";
+	
+	public static final String RELATIVE_LAYOUT = "RelativeLayout";
+	
+	public static final String LISTVIEW = "ListView";
+}
