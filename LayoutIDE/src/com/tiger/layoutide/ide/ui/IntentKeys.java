018991a3924ecfacaf1c64d6f5a9988aabf31f70
tiger <664674103@qq.com>
2014-11-03 17:46:47 +0800

    新增LAYOUT_NAME

--- LayoutIDE/src/com/tiger/layoutide/ide/ui/IntentKeys.java
+++ LayoutIDE/src/com/tiger/layoutide/ide/ui/IntentKeys.java
@@ -2,5 +2,9 @@ package com.tiger.layoutide.ide.ui;
 
 public class IntentKeys
 {
-	public static final String SELECTED_TEMPLATE_TYPE = "template_type";
+	public static final String TEMPLATE_TYPE = "template_type";
+	
+	public static final String LAYOUT_NAME = "layout_name";
+	
+	public static final String LAYOUT_TYPE = "layout_type";
 }
