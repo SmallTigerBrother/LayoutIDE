b3f8508e2040a3d501a99e827e064fd5126a7dbf
tiger <664674103@qq.com>
2014-11-04 17:51:31 +0800

    新增final，sycronized

--- JCodeGenerator/src/com/tiger/code/model/constant/JModifier.java
+++ JCodeGenerator/src/com/tiger/code/model/constant/JModifier.java
@@ -3,4 +3,8 @@ package com.tiger.code.model.constant;
 public class JModifier
 {
 	public static final String STATIC = "static ";
+	
+	public static final String SYNCHRONIZED = "synchronized ";
+	
+	public static final String FINAL = "final ";
 }
