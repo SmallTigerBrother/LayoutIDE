618028d6f4fb83097bd14c418701a275265fa849
tiger <664674103@qq.com>
2014-11-05 15:24:46 +0800

    新增extends、implements声明

--- JCodeGenerator/src/com/tiger/code/model/constant/JModifier.java
+++ JCodeGenerator/src/com/tiger/code/model/constant/JModifier.java
@@ -7,4 +7,8 @@ public class JModifier
 	public static final String SYNCHRONIZED = "synchronized ";
 	
 	public static final String FINAL = "final ";
+	
+	public static final String EXTENDS = "extends ";
+	
+	public static final String IMPLEMENTS = "implements ";
 }
