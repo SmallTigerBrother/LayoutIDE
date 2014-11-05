51e26a5344dca9b4a357d616ddc3680f937139b1
tiger <664674103@qq.com>
2014-11-05 17:35:04 +0800

    新增关键字类

---
+++ JCodeGenerator/src/com/tiger/code/model/constant/JKeyWords.java
@@ -0,0 +1,16 @@
+package com.tiger.code.model.constant;
+
+public class JKeyWords
+{
+	public static final String STATIC = "static ";
+	
+	public static final String SYNCHRONIZED = "synchronized ";
+	
+	public static final String FINAL = "final ";
+	
+	public static final String EXTENDS = "extends ";
+	
+	public static final String IMPLEMENTS = "implements ";
+	
+	public static final String SUPER = "super";
+}
