ff326c344cd6e3023dcab6be51d7966c2e9999d0
tiger <664674103@qq.com>
2014-11-11 17:44:54 +0800

    新增三个控制参数

--- LayoutIDE/src/com/tiger/layoutide/ide/code/OutputParams.java
+++ LayoutIDE/src/com/tiger/layoutide/ide/code/OutputParams.java
@@ -1,6 +1,53 @@
 package com.tiger.layoutide.ide.code;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tiger.code.model.JInterface;
+import com.tiger.layoutide.ide.code.library.AndroidInterface;
+import com.tiger.layoutide.ide.code.library.InterfaceFactory;
+
 public class OutputParams
 {
+	private String layoutName;
+	
+	private List<JInterface> interfaces;
+	
+	private boolean registerEventBus;
 
+	public OutputParams()
+	{
+		interfaces = new ArrayList<JInterface>();
+	}
+	
+	public void addInterface(AndroidInterface interface1)
+	{
+		JInterface realInterface = InterfaceFactory.createInterface(interface1);
+		interfaces.add(realInterface);
+	}
+	
+	public List<JInterface> getInterfaces()
+	{
+		return interfaces;
+	}
+
+	public String getLayoutName()
+	{
+		return layoutName;
+	}
+
+	public void setLayoutName(String layoutName)
+	{
+		this.layoutName = layoutName;
+	}
+
+	public boolean isRegisterEventBus()
+	{
+		return registerEventBus;
+	}
+
+	public void setRegisterEventBus(boolean registerEventBus)
+	{
+		this.registerEventBus = registerEventBus;
+	}
 }
