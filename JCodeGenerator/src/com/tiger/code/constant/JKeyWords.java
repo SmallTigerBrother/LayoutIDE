df58ff59daf8ae2c82af1e9a9c547dcd1e0476de
tiger <664674103@qq.com>
2014-11-12 16:21:17 +0800

    修改缩进

--- JCodeGenerator/src/com/tiger/code/constant/JKeyWords.java
+++ JCodeGenerator/src/com/tiger/code/constant/JKeyWords.java
@@ -2,15 +2,15 @@ package com.tiger.code.constant;
 
 public class JKeyWords
 {
-	public static final String STATIC = "static ";
+	public static final String STATIC = " static ";
 	
-	public static final String SYNCHRONIZED = "synchronized ";
+	public static final String SYNCHRONIZED = " synchronized ";
 	
 	public static final String FINAL = "final ";
 	
-	public static final String EXTENDS = "extends ";
+	public static final String EXTENDS = " extends ";
 	
-	public static final String IMPLEMENTS = "implements ";
+	public static final String IMPLEMENTS = " implements ";
 	
 	public static final String SUPER = "super";
 	
